b4a27cddacf98af377871de75cac389187fa3a3e
anaoli13jps <anaoli13jps@gmail.com>
2025-03-30 19:19:17 +0000

    primeiro commit , ent n esta tudo errado mas eu n to sabendo fazer commit. APRENDI!!!!!!!!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ export function App(){
 
         <h1>How did we do?</h1>
         <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+        <p>nada não so queria fazer o commit funcionar msm kkk</p>
 
         <div>
           <input type="button" value={1} />
