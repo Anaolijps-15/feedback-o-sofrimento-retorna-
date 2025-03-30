eb0631f6130f43737c114637f58ee6e882fd6070
anaoli13jps <anaoli13jps@gmail.com>
2025-03-30 23:10:26 +0000

    CABO!! (eu acho ) aeeee fim do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -27,22 +27,22 @@ function handleChangeRateNote(value){
               <p className="light-grey text-sm leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
       
               <div className="flex justify-between mb-6 ">
-                <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-Medium-gray font-bold focus:bg-Medium-gray focus:text-white"onClick={() => handleChangeRateNote(1)}/>
-                <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-Medium-gray font-bold focus:bg-Medium-gray focus:text-white"onClick={() => handleChangeRateNote(2)}/>
-                <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-Medium-gray font-bold focus:bg-Medium-gray focus:text-white"onClick={() => handleChangeRateNote(3)}/>
-                <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-Medium-gray font-bold focus:bg-Medium-gray focus:text-white"onClick={() => handleChangeRateNote(4)}/>
-                <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-Medium-gray font-bold focus:bg-Medium-gray focus:text-white"onClick={() => handleChangeRateNote(5)}/>
+                <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-Medium-gray font-bold focus:bg-Medium-gray focus:text-white hover:bg-orange hover:text-white cursor-pointer sn:w13 sn:h13 transition duration-400"onClick={() => handleChangeRateNote(1)}/>
+                <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-Medium-gray font-bold focus:bg-Medium-gray focus:text-white hover:bg-orange hover:text-white cursor-pointer sn:w13 sn:h13 transition duration-400"onClick={() => handleChangeRateNote(2)}/>
+                <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-Medium-gray font-bold focus:bg-Medium-gray focus:text-white hover:bg-orange hover:text-white cursor-pointer sn:w13 sn:h13 transition duration-400"onClick={() => handleChangeRateNote(3)}/>
+                <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-Medium-gray font-bold focus:bg-Medium-gray focus:text-white hover:bg-orange hover:text-white cursor-pointer sn:w13 sn:h13 transition duration-400"onClick={() => handleChangeRateNote(4)}/>
+                <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-Medium-gray font-bold focus:bg-Medium-gray focus:text-white hover:bg-orange hover:text-white cursor-pointer sn:w13 sn:h13 transition duration-400"onClick={() => handleChangeRateNote(5)}/>
              </div>
     
-             <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1" onClick={handleSubmit}>Submit</button>
+             <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1 hover:bg-white hover:text-orange cursor-pointer transition duration-400" onClick={handleSubmit}>Submit</button>
                 </div>
       ): (
-        <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass texte-center">
+        <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass text-center">
           <img className="mx-auto mb-6" src={imagemobg} alt="imagem gratiluz" />
 
-          <p className="mb-6 texte-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You selected {rateNote} out of 5</p>
+          <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You selected {rateNote} out of 5</p>
 
-          <h1 className="font-bold text-2xl mb-2.5">Thank You!</h1> 
+          <h1 className="font-bold text-2xl mb-2.5 ">Thank You!</h1> 
           <p className="light-grey text-sm leading-1 ">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
         </div>
       )
